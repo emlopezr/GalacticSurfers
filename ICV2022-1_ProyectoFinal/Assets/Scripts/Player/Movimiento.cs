6ce3b003af3a35ad16c2bc933aaff812092282cd
Lukas Duran <lduranz@unal.edu.co>
2022-05-16 16:07:38 -0500

    Adicion del script del movimiento

---
+++ ICV2022-1_ProyectoFinal/Assets/Scripts/Player/Movimiento.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    
+    private float zspeed = 10;
+    private float xyspeed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Desplazamiento en el eje Z
+        Vector3 _zdir = new Vector3(0,0,1);
+        Vector3 pos = transform.position;
+        Vector3 movement = zspeed * _zdir * Time.deltaTime;
+        pos += movement;
+        transform.position = pos;
+        
+        //Desplazamiento en el eje x,y
+        float horizontal= Input.GetAxisRaw("Horizontal");
+        float vertical= Input.GetAxisRaw("Vertical");
+        Vector3 _xydir = new Vector3(horizontal,vertical,0);
+        _xydir.Normalize();
+        Vector3 xypos = transform.position;
+        Vector3 xymovement = xyspeed * _xydir * Time.deltaTime;
+        xypos += xymovement;
+        transform.position = xypos;
+
+
+    }
+}
